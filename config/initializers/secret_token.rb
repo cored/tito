e341f9d25ac97bd79e71dfe7000693de79750246
Rafael George <george.rafael@gmail.com>
2013-12-26 19:27:34 -0400

    Add rspec and cucumber to Gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tito::Application.config.secret_key_base = '4dec5fa54cb2c74d2477e2691ac8cf9c854becac11d8022fb7d4258f2b9873f2e2695b93c6f4103c04763520eacf191548eeaa4db4886344076d7ee13b65f2eb'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+Tito::Application.config.secret_key_base = secure_token
