2ce1fa47524c0a3a683094e1f15d34e6a95a0c82
Rafael George <george.rafael@gmail.com>
2013-12-26 19:21:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tito::Application.config.secret_key_base = '4dec5fa54cb2c74d2477e2691ac8cf9c854becac11d8022fb7d4258f2b9873f2e2695b93c6f4103c04763520eacf191548eeaa4db4886344076d7ee13b65f2eb'
